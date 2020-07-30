c85f849d82e3d583d0cd3a8cdb606d9f17cd3023
bonasunu <bonaventurasunu@gmail.com>
2020-07-30 21:40:08 +0700

    build a test for front end

---
+++ src/components/Blog.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/react'
+import Blog from './Blog'
+
+test('renders content', () => {
+  const blog = {
+    title: 'Blog test title',
+    author: 'Blog test author'
+  }
+
+  const component = render(
+    <Blog blog={blog} />
+  )
+
+  expect(component.container).toHaveTextContent(
+    'Blog test title Blog test author'
+  )
+})
