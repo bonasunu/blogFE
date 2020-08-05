a9deda5ae97212bcb152ffe359c14736fdca2181
bonasunu <bonaventurasunu@gmail.com>
2020-08-05 23:21:29 +0700

    implement sorting blog by the number of likes

--- src/App.js
+++ src/App.js
@@ -154,10 +154,11 @@ const App = () => {
   const viewBlog = () => {
 
     return (
-      blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} handleLike={handleLike}>
-        </Blog>
-      )
+      blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog key={blog.id} blog={blog} handleLike={handleLike}>
+          </Blog>)
     )
   }
 
